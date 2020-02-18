7138a98669ad209886166decafe5c5bbcb1172fa
alexandre boceno <alexboceno@hotmail.fr>
2020-02-18 07:18:31 +0100

    wip

---
+++ index.js
@@ -0,0 +1,42 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "airbnb"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "import/extensions": 0,
+        "react/jsx-filename-extension": 0,
+        "react/jsx-props-no-spreading": 0,
+        "react/prop-types": 1,
+        "jsx-a11y/click-events-have-key-events": 0,
+        "import/prefer-default-export": 0,
+        "react/destructuring-assignment": 0,
+        "consistent-return": 0,
+        "import/no-unresolved": 0,
+        "no-nested-ternary": 0,
+        "sx-a11y/no-static-element-interactions": 0,
+        "no-shadow": 0,
+        "import/no-cycle": 0,
+        "react-hooks/exhaustive-deps": 0
+    }
+};
