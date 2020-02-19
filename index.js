d2ce12dbd4173f567a3d974bdb3282a94b4081ba
alexandre boceno <alexboceno@hotmail.fr>
2020-02-19 08:45:06 +0100

    fix linter

--- index.js
+++ index.js
@@ -33,10 +33,13 @@ module.exports = {
         "react/destructuring-assignment": 0,
         "consistent-return": 0,
         "import/no-unresolved": 0,
+        "jsx-a11y/no-static-element-interactions": 0,
         "no-nested-ternary": 0,
         "sx-a11y/no-static-element-interactions": 0,
         "no-shadow": 0,
         "import/no-cycle": 0,
-        "react-hooks/exhaustive-deps": 0
+        "react-hooks/exhaustive-deps": 0,
+        "no-plusplus": 0,
+        "no-param-reassign": 0
     }
 };
